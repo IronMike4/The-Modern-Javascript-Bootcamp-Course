90f5f9edc54966d1c198f96bdff3888f65bd3431
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 22:14:16 +0200

    slice

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-let letters = ['T','C','E','P','S','E','R'];
+let animals = ['shark','salmon','whale','bear','lizard','tortoise'];
 
-letters.reverse;
+
+let swimmers = animals.slice(0,3);
+
+let mammals = animals.slice(2,4);
+
+let reptiles = animals.slice(4);
+
+let quadruped = animals.slice(-3);

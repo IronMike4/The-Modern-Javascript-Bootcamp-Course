b2df7aeeff97f154b9483618db9a5f63d5099f9d
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 20:30:28 +0200

    Array Indices

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-let shoppingList = ['cereal','cheese','ice'];
+let colors = ['red','orange','yellow','green'];
+
+colors.length //4
+
+colors[0] //red
+colors[1] //orange
+colors[2] // yellow
+colors[3] //green
+colors[4] //undefined
+

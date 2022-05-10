55cedd203276a7adf8b18c903239a367d3caef5e
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 20:05:22 +0200

    Ternary operator

--- app.js
+++ app.js
@@ -1,69 +1,21 @@
-let day = 7;
+let num = 7;
 
-// if(day === 1){
-//     console.log('MONDAY');
-// }
-// else if (day === 2){
-//     console.log('TUESDAY');
-// }
-// else if (day === 3){
-//     console.log('WEDNESDAY');
-// }
-// else if (day === 4){
-//     console.log('THURSDAY');
-// }
-// else if (day === 5){
-//     console.log('FRIDAY');
-// }
-// else if (day === 6){
-//     console.log('SATURDAY');
-// }
-// else if (day === 7){
-//     console.log('SUNDAY');
+// if(num === 7) {
+//     console.log('lucky!');
 // }
 // else {
-//     console.log('INVALID DAY');
+//     console.log('bad!');
 // }
 
-// switch (day) {
-//     case 1:
-//         console.log('MONDAY');
-//         break;
-//     case 2:
-//         console.log('TUESDAY');
-//         break;
-//     case 3:
-//         console.log('WEDNESDAY');
-//         break;
-//     case 4:
-//         console.log('THURSDAY');
-//         break;
-//     case 5:
-//         console.log('FRIDAY');
-//         break;
-//     case 6:
-//         console.log('SATURDAY');
-//         break;
-//     case 7:
-//         console.log('SUNDAY');
-//         break;
-//     default:
-//         console.log('INVALID DAY');
-// }
+// num === 7 ? console.log('lucky!') : console.log('bad!');
 
 
-let emoji = 'heart';
+let statuss = 'offline';
 
-switch (emoji) {
-    case 'sad face':
-    case 'happy face':
-        console.log('yellow');
-        break;
-    case 'eggplant':
-        console.log('purple');
-        break;
-    case 'heart':
-    case 'lips':
-        console.log('red');
-        break;
+let color;
+if (statuss === 'offline') {
+    color = 'red';
+}
+else {
+    color = 'green';
 }

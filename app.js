217096accc4016fe3f72d9b64cfd746897228dca
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 15:36:19 +0200

    if, else if, else

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-if(1 === 1){
-    console.log("it's true!");
+// Performance Review
+// 3 - superstar
+// 2 meets expectations
+// 1 needs improvement
+// anything else - wtf?
+
+let rating = 3;
+if(rating === 3) {
+    console.log('YOU ARE A SUPERSTAR!')
+}
+else if (rating === 2){
+console.log('MEETS EXPECTATIONS');
+}
+else if(rating === 1){
+    console.log('NEEDS IMPROVEMENT');
+}
+else {
+    console.log('INVALID RATING')
 }
 
 // Example 2
 
-let rating = 2;
-if(rating < 3) {
-    console.log("YOU ARE NOT A SUPERSTAR!")
+let highScore = 1430;
+let userScore = 1800;
+
+if(userScore >= highScore){
+    console.log(`Congrats, you have the new high score of ${userScore}`);
+    highScore = userScore;
 }
-
-// Example 3 
- let num = 37;
-
- if(num % 2  !== 0){
-     console.log('ODD NUMBER!');
- }
+else {
+    console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`
+);
+}

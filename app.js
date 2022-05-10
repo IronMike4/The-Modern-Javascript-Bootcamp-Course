d91abdc4b427a1584bf95a0f945c105df1a172cb
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 15:48:38 +0200

    nesting conditionals

--- app.js
+++ app.js
@@ -1,33 +1,13 @@
-// Performance Review
-// 3 - superstar
-// 2 meets expectations
-// 1 needs improvement
-// anything else - wtf?
+let password = 'kitty';
 
-let rating = 3;
-if(rating === 3) {
-    console.log('YOU ARE A SUPERSTAR!')
-}
-else if (rating === 2){
-console.log('MEETS EXPECTATIONS');
-}
-else if(rating === 1){
-    console.log('NEEDS IMPROVEMENT');
+if (password.length >= 6){
+    if (password.indexOf(' ') === -1){
+        console.log('Valid Password!');
+    }
+    else {
+        console.log('Password is long enough but can not contain spaces');
+    }
 }
 else {
-    console.log('INVALID RATING')
-}
-
-// Example 2
-
-let highScore = 1430;
-let userScore = 1800;
-
-if(userScore >= highScore){
-    console.log(`Congrats, you have the new high score of ${userScore}`);
-    highScore = userScore;
-}
-else {
-    console.log(`Good Game. Your score of ${userScore} did not beat the high score of ${highScore}`
-);
+    console.log('Password must be longer!');
 }

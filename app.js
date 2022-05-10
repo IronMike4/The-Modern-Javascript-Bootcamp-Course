416a450dbc5394c3dfe9f4ef5fcf45ebb7354e44
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-10 19:20:56 +0200

    Operator Precedence

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-let loggedInUser = 'Iwe';
-
-// If there isn't a logged in user
-
-if(!loggedInUser){
-    console.log('Get out of here!');
-}
-
-let flavor = 'banana';
-if(flavor !== 'grape' && flavor !== 'cherry') {
-    console.log("We don't have that flavor!");
-}
-
-if(!(flavor === 'grape' || flavor === 'cherry')){
-    console.log('Not one of the flavors');
-}
+let x = 7;
+(x == 7 || x ===3) && x > 10;
+console.log(x);

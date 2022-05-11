d7b223c8945c8240d502693507d4f8a1fb8c1b6a
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-11 19:30:56 +0200

    Objects

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const board = [
-    ['0',null,'X'],
-    [null, 'X','0'],
-    ['X','0',null]
-]
+// totalSteps -> 308727,
+// totalMiles -> 211.7,
+// avgCalorieBurn -> 5755,
+// workoutsThisWeek -> '5 of 7',
+// avgGoodSleep -> '2:13's
+
+const fitBitData = {
+    totalSteps : 308727,
+    totalMiles : 211.7,
+    avgCalorieBurn : 5755,
+    workoutsThisWeek : '5 of 7',
+    avgGoodSleep : '2:13'
+}

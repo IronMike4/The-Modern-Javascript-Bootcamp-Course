d9fb69d3002f2003191ee5bd2b1653d5c44819c4
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-13 18:26:23 +0200

    For loops

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-let nums = [ 1, 2, 3];
-let mystery = [1, 2, 3];
-let moreNums = nums;
+// for(let i = 1; i <= 10; i++){
+//     console.log('Hello:', i);
+// }
 
-// Not equal because they are stored in different references
-nums === mystery;
-moreNums === nums;
+// for(let i = 1; i <= 10; i+=3){
+//     console.log('Hello:', i);
+// }
+
+// 1 1x1
+// 4 2x2
+// 9 3x3
+// 16 4x4
+// 25 5x5
+// 36 6x6
+// 49 7x7
+
+// for(let num = 1; num <= 20; num++){
+//     console.log(`${num}x${num} = ${num * num}`);
+// }
+
+for(let i = 200; i >= 0; i-=25){
+    console.log(i);
+}

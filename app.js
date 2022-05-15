12f3da19c3c81cc419f97620454720385a8662e7
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 08:44:42 +0200

    Break Keyword

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const target = Math.floor(Math.random() * 10);
 let guess = Math.floor(Math.random() * 10);
-while(guess !== target){
+while(true){
+    if(target === guess) break
     console.log(`Target: ${target} Guess: ${guess}`);
     guess = Math.floor(Math.random() * 10); 
 }
+
 console.log(`Target: ${target} Guess: ${guess}`);
 console.log(`CONGRATS YOU WIN!`);
-

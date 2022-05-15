e57f96b0b4ecf0b7525edb8f483e9e14244c5f44
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 11:36:52 +0200

    More return values

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-function add(x,y){
-    return x + y;
+// Only runs upto return statement
+function square(x){
+    return x * x;
+    console.log('All Done');
 }
+
+// function isPurple(color){
+//     if(color.toLowerCase() === 'purple'){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// function isPurple(color){
+//     if(color.toLowerCase() === 'purple'){
+//         return true;
+//     }
+//     return false;
+// }
+
+function isPurple(color){
+    return color.toLowerCase() === 'purple';
+}
+
+

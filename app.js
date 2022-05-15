69d7b7a60b96fce77c9a2d548f63bb84800a379e
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 21:13:26 +0200

    Arrow functions intro

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const numbers = [20,21,22,23,24,25,26,27];
-const words = ['asap','byob','rsvp','diy'];
+// const square = function(x){
+//     return x * x;
+// }
 
-const doubles = numbers.map(function(num){
-    return num * 2;
-})
+const square = (x) =>{
+    return x * x;
+}
 
-const numDetail = numbers.map(function(n){
-    return {
-        value: n,
-        isEven: n % 2 === 0
-    }
-})
+const isEven = (num) => {
+    return num % 2 === 0;
 
-const abbrevs = words.map(function(word){
-    return word.toUpperCase().split('').join('.')
-})
+}
+
+const multiply = (x,y) => {
+    return x * y;
+}

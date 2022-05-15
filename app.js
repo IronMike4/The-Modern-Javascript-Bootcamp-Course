0fc8dc93e1a468b0032101274048cb1db4d3639b
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 20:59:40 +0200

    Map

--- app.js
+++ app.js
@@ -1,52 +1,17 @@
 const numbers = [20,21,22,23,24,25,26,27];
+const words = ['asap','byob','rsvp','diy'];
 
-// numbers.forEach(function(num){
-//     console.log(num * 2);
-
-// })
-
-// function printTriple(n){
-//     console.log(n * 3);
-// }
-
-// numbers.forEach(printTriple);
-
-numbers.forEach(function(num,idx){
-    console.log(idx,num)
+const doubles = numbers.map(function(num){
+    return num * 2;
 })
 
-const books = [{
-    title: 'Good Omens',
-    authors: ['Terry Pratchett','Neil Gaiman'],
-    rating: 4.25
-},
-{
-    title: 'Bone: The Complete Edition',
-    authors:['Jeff Smith'],
-    rating: 4.42
-},
-{
-    title: 'American Gods',
-    authors: ['Neil Gaiman'],
-    rating: 4.11
-},
-{
-    title:'A Gentleman in Moscow',
-    authors:['Amor Towles'],
-    rating: 4.36
-}
-]
+const numDetail = numbers.map(function(n){
+    return {
+        value: n,
+        isEven: n % 2 === 0
+    }
+})
 
-// books.forEach(function(book){
-//     console.log(book.title.toUpperCase())
-// })
-
-// // You can achieve the same with for of
-// for(let book of books){
-//     console.log(book.title.toUpperCase())
-// }
-
-// // You can achieve the same with for loop
-// for (let i = 0; i < books.length; i++){
-//     console.log(books[i].title.toUpperCase())
-// }
+const abbrevs = words.map(function(word){
+    return word.toUpperCase().split('').join('.')
+})

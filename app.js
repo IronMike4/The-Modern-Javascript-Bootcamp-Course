6a4fc73246e23f6188040dd60c2c4d0f40539928
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 20:38:46 +0200

    forEach

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-// console.log(animal);
-// var animal = 'Tapir';
+const numbers = [20,21,22,23,24,25,26,27];
 
-// console.log(shrimp);
-// let shrimp = 'Harlequin Shrimp';
+// numbers.forEach(function(num){
+//     console.log(num * 2);
 
-// how();
-// function how(){
-//     console.log("Awoooo");
+// })
+
+// function printTriple(n){
+//     console.log(n * 3);
 // }
 
-hoot();
-var hoot = function (){
-    console.log('Hoooo Hoooo')
+// numbers.forEach(printTriple);
+
+numbers.forEach(function(num,idx){
+    console.log(idx,num)
+})
+
+const books = [{
+    title: 'Good Omens',
+    authors: ['Terry Pratchett','Neil Gaiman'],
+    rating: 4.25
+},
+{
+    title: 'Bone: The Complete Edition',
+    authors:['Jeff Smith'],
+    rating: 4.42
+},
+{
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11
+},
+{
+    title:'A Gentleman in Moscow',
+    authors:['Amor Towles'],
+    rating: 4.36
 }
+]
 
+// books.forEach(function(book){
+//     console.log(book.title.toUpperCase())
+// })
 
+// // You can achieve the same with for of
+// for(let book of books){
+//     console.log(book.title.toUpperCase())
+// }
+
+// // You can achieve the same with for loop
+// for (let i = 0; i < books.length; i++){
+//     console.log(books[i].title.toUpperCase())
+// }

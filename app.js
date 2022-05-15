d634a6238f7c71b16d7b4eaa68be8052cd979df4
Michael Mahachi <Michael.Mahachi@derivco.co.za>
2022-05-15 16:28:17 +0200

    Lexical Scope

--- app.js
+++ app.js
@@ -1,36 +1,12 @@
-// if(true) {
-//     const animal = 'eel';
-//     console.log(animal);
-// }
-// console.log(animal);
+function outer(){
+    let movie = 'Amadeus';
+    function inner(){
+        let movie = 'The Shining';
 
-
-// if(true) {
-//     var animal = 'eel';
-//     console.log(animal);
-// }
-// console.log(animal);
-
-// let animals = ['grizzly bear','panda bear','spectacled bear'];
-// var i = 10;
-// for (var i = 0; i < animals.length; i++){
-//     console.log(i, animals[i])
-// }
-// console.log(i);
-
-// let animals = ['grizzly bear','panda bear','spectacled bear'];
-// let i = 10;
-// for (let i = 0; i < animals.length; i++){
-//     console.log(i, animals[i])
-// }
-// console.log(i);
-
-function doubleArr(arr){
-    const result = [];
-    for (let num of arr){
-        var double = num * 2;
-        result.push(double);
+        function extraInner(){
+        console.log(movie.toUpperCase());
+        }
+        extraInner();
     }
-    console.log(double);
-    return result;
+    inner();
 }
